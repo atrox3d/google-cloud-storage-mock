33f3a34d8afbdfc1cebc6a51a1b53c530b784678
atrox3d <atrox3d@gmail.com>
2025-12-04 17:56:17 +0100

    add test_new_bucket_auto_create_dirs

--- tests/google/cloud/test_pytest.py
+++ tests/google/cloud/test_pytest.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 logging.basicConfig(
     format='%(asctime)s | %(levelname)-8s | %(module)10s | %(funcName)15s | %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
@@ -9,7 +10,21 @@ from google.cloud import storage
 
 logger = logging.getLogger(__name__)
 
-def test_1():
+def test_sys_path():
     import sys
-    print('hello', sys.path)
-    logger.warning(sys.path)
+    logger.info(sys.path)
+
+
+def test_new_bucket_auto_create_dirs():
+    logger.info(f'{storage.FAKE_BUCKETS_ROOT = }')
+    BUCKET_DIRNAME = 'test-bucket'
+
+    bucket_path = Path(storage.FAKE_BUCKETS_ROOT) / BUCKET_DIRNAME
+    bucket_path.exists() and bucket_path.rmdir()
+    assert bucket_path.exists() == False
+
+    bucket = storage.Bucket(BUCKET_DIRNAME, auto_create_dirs=True)
+    logger.info(f'{bucket.path = }')
+    assert bucket.path == bucket_path
+    assert bucket.path.exists()
+    assert bucket.path.is_dir()
