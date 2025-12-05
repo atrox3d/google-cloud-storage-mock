5d54ee527edd10a26e8553e69af921ccd8386698
atrox3d <atrox3d@gmail.com>
2025-12-05 15:31:17 +0100

    add parametric fixtures examples

---
+++ tests/google/cloud/test_fixtures.py
@@ -0,0 +1,44 @@
+import pytest
+
+
+@pytest.fixture
+def fixture_noparams(request):
+    print(f'{request = }')
+    # print(f'{request.param = }')
+
+
+def test_fixture_no_param(fixture_noparams):
+    pass
+
+
+@pytest.fixture
+def fixture_oneparam(request):
+    print(f'{request = }')
+    print(f'{request.param = }')
+    yield request.param
+
+
+
+@pytest.mark.parametrize(
+    'fixture_oneparam',
+    ['hi'],
+    indirect=True
+)
+def test_fixture_one_param(fixture_oneparam):
+    print(f'{fixture_oneparam = }')
+
+
+@pytest.fixture
+def fixture_twoparams(request):
+    print(f'{request = }')
+    print(f'{request.param = }')
+    yield request.param
+
+
+@pytest.mark.parametrize(
+    'fixture_twoparams',
+    [('hi', 'there')],
+    indirect=True
+)
+def test_fixture_two_params(fixture_twoparams):
+    print(f'{fixture_twoparams = }')
